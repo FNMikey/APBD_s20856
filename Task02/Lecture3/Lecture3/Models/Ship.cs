e8d9829f58b5a820354f9c4a942eb2cf8e018375
Michal Konieczny <s20856@pjwstk.edu.pl>
2024-03-29 20:19:41 +0100

    Creating Ship class

--- Task02/Lecture3/Lecture3/Models/Ship.cs
+++ Task02/Lecture3/Lecture3/Models/Ship.cs
@@ -6,12 +6,85 @@ using System.Threading.Tasks;
 
 namespace Lecture3.Models
 {
-    internal class Ship
+    public class Ship
     {
-        public List<Container> Containers { get; set; }
+        public List<Container> Containers { get; private set; }
         public double MaxSpeed { get; set; }
         public int MaxContainerCount { get; set; }
         public double MaxLoadWeight { get; set; }
 
+        public Ship(double maxSpeed, int maxContainerCount, double maxLoadWeight)
+        {
+            MaxSpeed = maxSpeed;
+            MaxContainerCount = maxContainerCount;
+            MaxLoadWeight = maxLoadWeight;
+            Containers = new List<Container>();
+        }
+
+        public void AddContainer(Container container)
+        {
+            if (Containers.Count >= MaxContainerCount)
+            {
+                throw new InvalidOperationException("Cannot add more containers: maximum capacity reached.");
+            }
+
+            double totalWeight = Containers.Sum(c => c.LoadMass + c.OwnWeight);
+            if (totalWeight > MaxLoadWeight)
+            {
+                throw new InvalidOperationException("Cannot add the container: exceeding maximum load weight.");
+            }
+
+            Containers.Add(container);
+        }
+
+        public bool RemoveContainer(string serialNumber)
+        {
+            var container = Containers.FirstOrDefault(c => c.SerialNumber == serialNumber);
+            if (container != null)
+            {
+                Containers.Remove(container);
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool ReplaceContainer(string serialNumber, Container newContainer)
+        {
+            int index = Containers.FindIndex(c => c.SerialNumber == serialNumber);
+            if (index != -1)
+            {
+                Containers[index] = newContainer;
+                return true;
+            }
+
+            return false;
+        }
+
+
+
+        public void PrintShipInfo()
+        {
+            Console.WriteLine($"Informacje o statku:");
+            Console.WriteLine($"Maksymalna prędkość: {MaxSpeed}");
+            Console.WriteLine($"Maksymalna liczba kontenerów: {MaxContainerCount}");
+            Console.WriteLine($"Maksymalna waga ładunku: {MaxLoadWeight}");
+            Console.WriteLine($"Aktualna liczba kontenerów: {Containers.Count}");
+            Console.WriteLine($"Aktualna waga ładunku: {Containers.Sum(c => c.LoadMass + c.OwnWeight)}");
+
+            Console.WriteLine($"Lista kontenerów:");
+
+            if (Containers.Count == 0)
+            {
+                Console.WriteLine("Brak kontenerów na statku.");
+                return;
+            }
+
+            foreach (var container in Containers)
+            {
+                Console.WriteLine(container.ToString());
+            }
+        }
     }
+
 }
--- Task02/Lecture3/Lecture3/Program.cs
+++ Task02/Lecture3/Lecture3/Program.cs
@@ -5,7 +5,10 @@ namespace Lecture3;
 public class Program
 {
     public static void Main(string[] args)
+
     {
+
+        Ship ship = new Ship(100, 4, 30000);
 
         CooledContainer cooledContainer = new CooledContainer(100, 100, 100 , 1000, CooledContainer.ProductType.Butter);
         Console.WriteLine(cooledContainer.ToString());
@@ -16,7 +19,12 @@ public class Program
         LiquidContainer liquidContainer = new LiquidContainer(100, 100, 100, 100, true);
         Console.WriteLine(liquidContainer.ToString());
 
+        ship.AddContainer(gasContainer);
+        ship.AddContainer(liquidContainer);
+        ship.AddContainer(cooledContainer);
+
+        ship.PrintShipInfo();
 
 
-    }
+      }
 }
