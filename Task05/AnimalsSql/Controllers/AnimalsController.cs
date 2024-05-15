555050685709643b85307c0764bee105ab9fc332
Michal Konieczny <s20856@pjwstk.edu.pl>
2024-05-15 20:46:51 +0200

    Created Anima and IAnimalsRepository

---
+++ Task05/AnimalsSql/Controllers/AnimalsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AnimalsSql.Controllers
+{
+    public class AnimalsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Task05/AnimalsSql/Repositories/IAnimalsRepository.cs
@@ -0,0 +1,18 @@
+﻿using AnimalsSql.Models;
+
+namespace AnimalsSql.Repositories
+{
+    public interface IAnimalsRepository
+    {
+        IEnumerable<Animal> GetAnimals();
+
+        int CreateAnimal(Animal animal);
+
+        int UpdateAnimal(Animal animal);
+
+        int DeleteAnimal(Animal animal);
+
+
+
+    }
+}
